422b4e7d94a61bcbae12c09c747f1ecf312b52b5
GuillaumePevrier <guillaumepevrier@gmail.com>
2023-05-24 10:14:13 +0200

    Version 0.0.1

--- controller/clubController.js
+++ controller/clubController.js
@@ -26,12 +26,22 @@ exports.getClubById = async (req, res) => {
 };
 
 // Créer un club
+const path = require('path');
+
 exports.createClub = async (req, res) => {
   const { nom, adresse, histoire, joueurId } = req.body;
 
   let imageClub = '';
+  let fileName = '';
+
   if (req.file) {
-    imageClub = req.file.path;
+    // Génération du nom de fichier unique comme expliqué précédemment
+
+    // Enregistrement de l'image dans le répertoire approprié (par exemple, le dossier "images")
+    fileName = req.file.filename;
+    const imagePath = path.join('images', fileName);
+    req.file.path = imagePath; // Met à jour le chemin d'accès avec le chemin relatif
+    imageClub = imagePath;
   }
 
   const club = new Club({
@@ -44,6 +54,13 @@ exports.createClub = async (req, res) => {
 
   try {
     const nouveauClub = await club.save();
+
+    // Récupération de l'URL complète de l'image
+    const imageUrl = `${req.protocol}://${req.get('host')}/${imageClub.replace(/\.(jpg|png)$/, '.webp')}`;
+    nouveauClub.imageClub = imageUrl; // Met à jour la propriété imageClub avec l'URL complète
+
+    await nouveauClub.save(); // Enregistre à nouveau le club avec l'URL mise à jour
+
     res.status(201).json(nouveauClub);
   } catch (error) {
     console.error('Une erreur est survenue lors de la création du club:', error);
@@ -51,21 +68,56 @@ exports.createClub = async (req, res) => {
   }
 };
 
+const fs = require('fs');
+
+
 // Mettre à jour un club
 exports.updateClub = async (req, res) => {
   const { id } = req.params;
   const updates = req.body;
   try {
-    const club = await Club.findByIdAndUpdate(id, updates, { new: true });
+    const club = await Club.findById(id);
     if (!club) {
-      res.status(404).json({ error: 'Club introuvable.' });
-    } else {
-      res.status(200).json(club);
+      return res.status(404).json({ error: 'Club introuvable.' });
     }
+
+    let imageClub = club.imageClub;
+    let fileName = '';
+
+    if (req.file) {
+      // Génération du nom de fichier unique comme expliqué précédemment
+
+      // Enregistrement de la nouvelle image dans le répertoire approprié (par exemple, le dossier "images")
+      fileName = req.file.filename;
+      const imagePath = path.join('images', fileName);
+      req.file.path = imagePath; // Met à jour le chemin d'accès avec le chemin relatif
+      imageClub = imagePath;
+
+      // Suppression de l'ancienne image
+      if (fs.existsSync(path.join('images', path.basename(club.imageClub)))) {
+        fs.unlinkSync(path.join('images', path.basename(club.imageClub)));
+      }
+    }
+
+    club.set(updates);
+    club.imageClub = imageClub;
+
+    const updatedClub = await club.save();
+
+    // Récupération de l'URL complète de la nouvelle image
+    const imageUrl = `${req.protocol}://${req.get('host')}/${imageClub.replace(/\.jpg$/, '.webp')}`;
+
+    updatedClub.imageClub = imageUrl; // Met à jour la propriété imageClub avec l'URL complète
+
+    await updatedClub.save(); // Enregistre à nouveau le club avec l'URL mise à jour
+
+    res.status(200).json(updatedClub);
   } catch (error) {
+    console.error('Une erreur est survenue lors de la mise à jour du club:', error);
     res.status(500).json({ error: 'Une erreur est survenue lors de la mise à jour du club.' });
   }
 };
+
 
 // Supprimer un club
 exports.deleteClub = async (req, res) => {
--- controller/equipeController.js
+++ controller/equipeController.js
@@ -1,70 +1,122 @@
+const path = require('path');
+const fs = require('fs');
 const Equipe = require('../models/Equipe');
 
 // Récupérer toutes les équipes
 exports.getAllEquipes = async (req, res) => {
-	try {
-		const equipes = await Equipe.find().populate('joueurs matchs club');
-		res.status(200).json(equipes);
-	} catch (error) {
-		res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des équipes.' });
-	}
+  try {
+    const equipes = await Equipe.find().populate('joueurs matchs club');
+    res.status(200).json(equipes);
+  } catch (error) {
+    res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des équipes.' });
+  }
 };
 
 // Récupérer une équipe par son identifiant
 exports.getEquipeById = async (req, res) => {
-	const { id } = req.params;
-	try {
-		const equipe = await Equipe.findById(id).populate('joueurs matchs club');
-		if (!equipe) {
-			res.status(404).json({ error: 'Équipe introuvable.' });
-		} else {
-			res.status(200).json(equipe);
-		}
-	} catch (error) {
-		res.status(500).json({ error: "Une erreur est survenue lors de la récupération de l'équipe." });
-	}
+  const { id } = req.params;
+  try {
+    const equipe = await Equipe.findById(id).populate('joueurs matchs club');
+    if (!equipe) {
+      res.status(404).json({ error: 'Équipe introuvable.' });
+    } else {
+      res.status(200).json(equipe);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Une erreur est survenue lors de la récupération de l'équipe." });
+  }
 };
 
 // Créer une équipe
 exports.createEquipe = async (req, res) => {
-	const { nom, categorieAge, joueurs, matchs, imageEquipe, club } = req.body;
-	const equipe = new Equipe({ nom, categorieAge, joueurs, matchs, imageEquipe, club });
-	try {
-		const nouvelleEquipe = await equipe.save();
-		res.status(201).json(nouvelleEquipe);
-	} catch (error) {
-		res.status(500).json({ error: "Une erreur est survenue lors de la création de l'équipe." });
-	}
+  const { nom, categorieAge, joueurs, matchs, club } = req.body;
+
+  let imageEquipe = '';
+  let fileName = '';
+
+  if (req.file) {
+    fileName = req.file.filename;
+    const imagePath = path.join('images', fileName);
+    req.file.path = imagePath;
+    imageEquipe = imagePath;
+  }
+
+  const equipe = new Equipe({ nom, categorieAge, joueurs, matchs, imageEquipe, club });
+
+  try {
+    const nouvelleEquipe = await equipe.save();
+
+    const imageUrl = `${req.protocol}://${req.get('host')}/${imageEquipe.replace(/\.(jpg|png)$/, '.webp')}`;
+    nouvelleEquipe.imageEquipe = imageUrl;
+
+    await nouvelleEquipe.save();
+
+    res.status(201).json(nouvelleEquipe);
+  } catch (error) {
+    console.error("Une erreur est survenue lors de la création de l'équipe :", error);
+    res.status(500).json({ error: "Une erreur est survenue lors de la création de l'équipe." });
+  }
 };
 
 // Mettre à jour une équipe
 exports.updateEquipe = async (req, res) => {
-	const { id } = req.params;
-	try {
-		const equipe = await Equipe.findByIdAndUpdate(id, req.body, { new: true }).populate('joueurs matchs club');
-		if (!equipe) {
-			res.status(404).json({ error: 'Équipe introuvable.' });
-		} else {
-			res.status(200).json(equipe);
-		}
-	} catch (error) {
-		res.status(500).json({ error: "Une erreur est survenue lors de la mise à jour de l'équipe." });
-	}
+  const { id } = req.params;
+  const updates = req.body;
+  try {
+    const equipe = await Equipe.findById(id);
+    if (!equipe) {
+      return res.status(404).json({ error: 'Équipe introuvable.' });
+    }
+
+    let imageEquipe = equipe.imageEquipe;
+    let fileName = '';
+
+    if (req.file) {
+      fileName = req.file.filename;
+      const imagePath = path.join('images', fileName);
+      req.file.path = imagePath;
+      imageEquipe = imagePath;
+
+      if (fs.existsSync(path.join('images', path.basename(equipe.imageEquipe)))) {
+        fs.unlinkSync(path.join('images', path.basename(equipe.imageEquipe)));
+      }
+    }
+
+    equipe.set(updates);
+    equipe.imageEquipe = imageEquipe;
+
+    const updatedEquipe = await equipe.save();
+
+    const imageUrl = `${req.protocol}://${req.get('host')}/${imageEquipe.replace(/\.jpg$/, '.webp')}`;
+
+    updatedEquipe.imageEquipe = imageUrl;
+
+    await updatedEquipe.save();
+
+    res.status(200).json(updatedEquipe);
+  } catch (error) {
+    console.error("Une erreur est survenue lors de la mise à jour de l'équipe :", error);
+    res.status(500).json({ error: "Une erreur est survenue lors de la mise à jour de l'équipe." });
+  }
 };
 
 // Supprimer une équipe
 exports.deleteEquipe = async (req, res) => {
-	const { id } = req.params;
-	try {
-		const equipe = await Equipe.findByIdAndRemove(id).populate('joueurs matchs club');
-		if (!equipe) {
-			res.status(404).json({ error: 'Équipe introuvable.' });
-		} else {
-			res.status(200).json({ message: 'Équipe supprimée avec succès.' });
-		}
-	} catch (error) {
-		res.status(500).json({ error: "Une erreur est survenue lors de la suppression de l'équipe." });
-	}
+  const { id } = req.params;
+  try {
+    const equipe = await Equipe.findByIdAndRemove(id);
+    if (!equipe) {
+      res.status(404).json({ error: 'Équipe introuvable.' });
+    } else {
+      if (fs.existsSync(path.join('images', path.basename(equipe.imageEquipe)))) {
+        fs.unlinkSync(path.join('images', path.basename(equipe.imageEquipe)));
+      }
+      res.status(200).json({ message: 'Équipe supprimée avec succès.' });
+    }
+  } catch (error) {
+    console.error("Une erreur est survenue lors de la suppression de l'équipe :", error);
+    res.status(500).json({ error: "Une erreur est survenue lors de la suppression de l'équipe." });
+  }
 };
 
 module.exports = exports;
